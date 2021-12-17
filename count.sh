19d75209e33b23be62a95bdc077d718eb2e20541
akshayvdes <vd.akshaykumar@gmail.com>
2021-12-17 17:13:04 +0000

    sum of integer

--- count.sh
+++ count.sh
@@ -2,4 +2,12 @@
 echo "enter a integer"
 read num
 var=` echo "$num" | wc -c`
-echo "lenght of cinteger is $var"
+echo "lenght of integer is $var"
+sum=0
+while [ $var -gt 0 ]
+do
+	var1=` echo "$num" | cut -c $var`
+	sum=` expr $sum + $var1`
+	var=` expr $var - 1`
+done
+echo "sum of integer is $sum"
